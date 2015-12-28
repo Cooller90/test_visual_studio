a0b5a199cb6090f8fdf3bc9ec9c128eb06a56ee8
Cooller <kfmgtu.kea@gmail.com>
2015-12-28 10:39:06 +0400

    Добавлен новый метод

--- 4git/4git/Program.cs
+++ 4git/4git/Program.cs
@@ -8,10 +8,18 @@ namespace _4git
 {
     class Program
     {
+        static int Sum(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
             Console.WriteLine("Hello world");
+            int Add = Sum(3, 4);
+            Console.WriteLine(Sum(4,5));
         }
+
     }
 }
